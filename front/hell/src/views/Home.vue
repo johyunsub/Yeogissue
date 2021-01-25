6025792af8c3bb7b5118f5d7d97f1222726f665b
DONGKYUDT\dongkyupc <adkgold77@naver.com>
2021-01-25 14:15:48 +0900

    Revert "add :  join"
    This reverts commit 87e036fa017abaf8bee13fe6ef5afa62ca8f496c.

--- front/hell/src/views/Home.vue
+++ front/hell/src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
+<<<<<<< HEAD
   <v-container>
     <issue/>
     <opinion-slider/>
     <suggest-slider/>
   </v-container>
+=======
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+>>>>>>> a78e6030ef30d1e78791c04fd4fbc81c75a5c125
 </template>
 
 <script>
