5015690af967385bd8c32c3608a4ac5e1ca64951
이대헌 <eogjs4818@naver.com>
2021-01-25 15:59:44 +0900

    add : 이슈 라벨 생성

--- back/issue/views.py
+++ back/issue/views.py
@@ -1,32 +1,71 @@
 from django.shortcuts import render
 import requests, json
-from urllib.parse import urlencode
-
+from .models import Dailyissue
 # Create your views here.
-def issuemaker():
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.db.models import Q
 
-    # URL 지정
+@api_view(['POST'])
+def issuemaker(request):
+    date = request.data.get('date')
+    category = request.data.get('category')
+    # if Dailyissue.objects.filter(Q(date=date) & Q(category=category)).exists():
+    #     data = Dailyissue.objects.filter(Q(date=date) & Q(category=category))
+    #     return Response(json(data))
+    # else:
+        
+        
+        # # URL 지정
+        # url = 'http://svc.saltlux.ai:31781'
+
+        # # Header 정보 지정
+        # headers = {'Content-Type': 'application/json; charset:utf-8'}
+
+        # # Request Parameter 정보 지정
+        # params ={
+        #     'key': 'fc95de29-1c01-4271-b099-56ffb2e8144d',
+        #     'serviceId': '11484686567',
+        #     'argument': {
+        #         "date": date,
+        #         "category": category
+        #     }
+        # }
+
+        # response = json.loads(requests.post(url, headers=headers, data=json.dumps(params)).text)
+        # #-*-coding: utf-8
+        # # print(response)
+        # k = 1
+        # for i in response['return_object']['topics']:
+        #     print(k,i['label'])
+        #     data = Dailyissue()
+        #     data.date = date
+        #     data.category = category
+        #     data.content = i['label']
+        #     data.save()
+        #     k += 1
+        # result = Dailyissue.objects.filter(Q(date=date) & Q(category=category))
+        # return Response(result)
+
     url = 'http://svc.saltlux.ai:31781'
 
-    # Header 정보 지정
     headers = {'Content-Type': 'application/json; charset:utf-8'}
 
-    # Request Parameter 정보 지정
     params ={
         'key': 'fc95de29-1c01-4271-b099-56ffb2e8144d',
         'serviceId': '11484686567',
         'argument': {
-            "date": "2021-01-21",
-            "category": "ENTERTAINMENT"
+            "date": date,
+            "category": category
         }
     }
 
     response = json.loads(requests.post(url, headers=headers, data=json.dumps(params)).text)
-    #-*-coding: utf-8
-    # print(response)
+
+    result = []
     k = 1
     for i in response['return_object']['topics']:
-        print(k,i['label'])
+        result.append({'date':date,'category':category,'content':i['label']})
         k += 1
-
-issuemaker()
+    return JsonResponse(result,safe=False,json_dumps_params={'ensure_ascii': False})
