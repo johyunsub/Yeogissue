87e036fa017abaf8bee13fe6ef5afa62ca8f496c
DONGKYUDT\dongkyupc <adkgold77@naver.com>
2021-01-25 14:15:34 +0900

    add :  join

--- front/hell/src/router/index.js
+++ front/hell/src/router/index.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-<<<<<<< HEAD
 import Issue from '../views/Issue/IssuePage.vue'
 import Opinion from '../views/Opinion/OpinionPage.vue'
 import Suggest from '../views/Suggest/SuggestPage.vue'
-=======
-import Login from '../views/Login.vue'
->>>>>>> a78e6030ef30d1e78791c04fd4fbc81c75a5c125
 
 Vue.use(VueRouter)
 
@@ -18,7 +14,6 @@ const routes = [
     component: Home
   },
   {
-<<<<<<< HEAD
     path: '/issue',
     name: 'Issue',
     component: Issue
@@ -34,12 +29,6 @@ const routes = [
     component: Suggest
   },
  
-=======
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
->>>>>>> a78e6030ef30d1e78791c04fd4fbc81c75a5c125
 ]
 
 const router = new VueRouter({
