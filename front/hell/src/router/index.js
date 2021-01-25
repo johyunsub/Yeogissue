7ae766ec09b719d6c5b540b09dbb4be49da9097a
GooWeonKang <fkdldjs487@gmail.com>
2021-01-25 15:32:40 +0900

    reset

--- front/hell/src/router/index.js
+++ front/hell/src/router/index.js
@@ -1,13 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-<<<<<<< HEAD
 import Issue from '../views/Issue/IssuePage.vue'
 import Opinion from '../views/Opinion/OpinionPage.vue'
 import Suggest from '../views/Suggest/SuggestPage.vue'
-=======
 import Login from '../views/Login.vue'
->>>>>>> a78e6030ef30d1e78791c04fd4fbc81c75a5c125
 
 Vue.use(VueRouter)
 
@@ -18,7 +15,6 @@ const routes = [
     component: Home
   },
   {
-<<<<<<< HEAD
     path: '/issue',
     name: 'Issue',
     component: Issue
@@ -33,13 +29,11 @@ const routes = [
     name: 'Suggest',
     component: Suggest
   },
- 
-=======
+  {
     path: '/login',
     name: 'Login',
     component: Login
   },
->>>>>>> a78e6030ef30d1e78791c04fd4fbc81c75a5c125
 ]
 
 const router = new VueRouter({
