25ca9816b7766eb83d3e91f729564e643c535bb7
DESKTOP-KVCQHCD\multicampus <gpwls2127@gmail.com>
2021-01-27 12:15:13 +0900

    add : 게시판 crud

--- back/server/settings.py
+++ back/server/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'issue',
     'accounts',
+    'articles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- back/server/urls.py
+++ back/server/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('issue/', include('issue.urls')),
+    path('articles/', include('articles.urls')),
 
 ]
