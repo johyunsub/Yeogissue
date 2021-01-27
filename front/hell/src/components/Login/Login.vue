537af52fad8b6052e43217c7d20b5d9ec6c476b7
DESKTOP-KVCQHCD\multicampus <hyunsub2167@gmail.com>
2021-01-27 17:16:09 +0900

    Update : front-backend 연동

--- front/hell/src/components/Login/Login.vue
+++ front/hell/src/components/Login/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row justify="center">
+  <div class="text-center">
     <v-dialog v-model="getDialog" persistent max-width="600px">
       <v-form ref="form" v-model="valid" lazy-validation>
         <v-card>
@@ -14,12 +14,14 @@
                     label="Email*"
                     required
                     :rules="emailRules"
+                    v-model="logindata.email"
                   ></v-text-field>
                 </v-col>
                 <v-col cols="12">
                   <v-text-field
                     label="Password*"
                     type="password"
+                    v-model="logindata.password"
                     hint="영문,숫자 포함 8자리 이상 적어주세요."
                     required
                   ></v-text-field>
@@ -34,6 +36,18 @@
                     >로그인</v-btn
                   >
                 </v-col>
+                <v-col v-if="accounts_valid" cols="12" sm="12">
+                  <v-alert
+                    dense
+                    border="left"
+                    type="warning"
+                    small
+                  >
+                    <strong>아이디</strong> 또는 <strong>비밀번호</strong>가 틀렸습니다
+                  </v-alert>
+                </v-col>
+
+                <v-divider></v-divider>
                 <v-col cols="12" sm="12">
                   <v-btn
                     tile
@@ -51,6 +65,7 @@
                   <v-btn
                     text
                     color="info"
+                    @click="forgotpw"
                     >비밀번호 찾기</v-btn
                   >
                 </v-col>
@@ -61,6 +76,7 @@
                   <v-btn
                     text
                     color="info"
+                    @click="gojoin"
                     >회원가입</v-btn
                   >
                 </v-col>
@@ -76,15 +92,21 @@
         </v-card>
       </v-form>
     </v-dialog>
-  </v-row>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     dialog: false,
-    valid: true,
-    email: "",
+    valid: false,
+    accounts_valid: false,
+    logindata: {
+      email : "",
+      password: "",
+    },
     emailRules: [
       (v) => !!v || "E-mail is required",
       (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
@@ -105,7 +127,17 @@ export default {
       this.$store.commit('CHANGE_DIALOG', false);
     },
     validate() {
-      this.$refs.form.validate();
+      axios //서버에 id, pw 요청
+        .post("http://127.0.0.1:8000/accounts/api-token-auth/", this.logindata)
+        .then((response) => {
+            this.OnOffLoign()
+            if(response.status == 200){
+              this.accounts_valid = false
+            }
+            console.log(response.status)
+            this.$router.push({ name: 'Home' });
+        });
+      this.accounts_valid = true
     },
     loginWithKakao() {
       const params = {
@@ -113,6 +145,12 @@ export default {
       };
       window.Kakao.Auth.authorize(params);
     },
+    forgotpw() {
+      this.$router.push({ name: 'Home' });
+    },
+    gojoin(){
+      this.$router.push({ name: 'Home' });
+    }
   },
 };
 </script>
