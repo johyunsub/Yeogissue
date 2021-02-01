9f8534686b17af914f59f8644825c306cde27e87
DESKTOP-KVCQHCD\multicampus <gpwls2127@gmail.com>
2021-02-01 21:50:59 +0900

    Add : 의견 나눔 댓글 crud 추가

--- back/articles/urls.py
+++ back/articles/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('article_list/', views.article_list),
     path('article_create/', views.article_create),
     path('<int:article_pk>/', views.article_detail),
-    path('hashtag/<int:article_pk>/',views.hashtag_create)
+    path('hashtag/<int:article_pk>/',views.hashtag_create),
 
+    path('<int:article_pk>/comments/', views.create_comment),
+    path('comments/', views.comment_list),
+    path('comments/<int:comment_pk>/', views.comment_detail_update_delete),
 ]
