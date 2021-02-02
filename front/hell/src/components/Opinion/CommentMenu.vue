d4aa2a4215daf162667410201af2cf4e6f783c79
GooWeonKang <fkdldjs487@gmail.com>
2021-02-02 17:38:31 +0900

    Add :CommentCreate, CommentMenu

---
+++ front/hell/src/components/Opinion/CommentMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-menu bottom left>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn icon v-bind="attrs" v-on="on">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item-title class="choice_cursor back-gray">{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [{ title: '수정' }, { title: '삭제' }, { title: '신고' }],
+  }),
+};
+</script>
--- front/hell/src/store/modules/opinionStore.js
+++ front/hell/src/store/modules/opinionStore.js
@@ -7,6 +7,7 @@ const opinionStore = {
   state: {
     opinions: null,
 
+    //페이징 하게 10개씩 저장
     opinionPaging: {},
     pagingCnt: 0,
 
@@ -17,6 +18,8 @@ const opinionStore = {
 
     //댓글
     opinionComment: null,
+    opinionCommentPaging: {},
+    opinionCommentPagingCnt: 0,
   },
   getters: {},
   mutations: {
@@ -24,7 +27,7 @@ const opinionStore = {
       state.opinions = opinions;
     },
 
-    SET_OPINIONPAGING(state, start) {
+    SET_OPINION_PAGING(state, start) {
       state.opinionPaging = {};
       state.pagingCnt = Math.floor(state.opinions.length / 10);
       if (state.opinions.length % 10 != 0) state.pagingCnt++;
@@ -35,8 +38,24 @@ const opinionStore = {
         state.opinionPaging[index++] = state.opinions[i];
       }
     },
-    SET_OPINIONDETAIL(state, opinion) {
+    SET_OPINION_DETAIL(state, opinion) {
       state.opinionData = opinion;
+    },
+
+    SET_OPINION_COMMENT(state, comment) {
+      state.opinionComment = comment;
+    },
+
+    SET_OPINION_COMMENT_PAGING(state, start) {
+      state.opinionCommentPaging = {};
+      state.opinionCommentPagingCnt = Math.floor(state.opinionComment.length / 10);
+      if (state.opinionComment.length % 10 != 0) state.opinionCommentPagingCnt++;
+
+      let index = 0;
+      for (let i = start; i < start + 10; i++) {
+        if (i == state.opinionComment.length) break;
+        state.opinionCommentPaging[index++] = state.opinionComment[i];
+      }
     },
   },
   actions: {
@@ -46,7 +65,7 @@ const opinionStore = {
         .get('/articles/article_list')
         .then((res) => {
           commit('SET_OPINIONS', res.data);
-          commit('SET_OPINIONPAGING', 0);
+          commit('SET_OPINION_PAGING', 0);
         })
         .catch((err) => console.log(err.response));
     },
@@ -65,7 +84,7 @@ const opinionStore = {
       instance
         .put(`/articles/${data.id}/`, data)
         .then(() => {
-          commit('SET_OPINIONDETAIL', data);
+          commit('SET_OPINION_DETAIL', data);
         })
         .catch((err) => console.log(err.response));
     },
@@ -75,7 +94,7 @@ const opinionStore = {
       instance
         .delete(`/articles/${id}/`)
         .then(() => {
-          commit('SET_OPINIONDETAIL', null);
+          commit('SET_OPINION_DETAIL', null);
         })
         .catch((err) => console.log(err.response));
     },
@@ -85,7 +104,19 @@ const opinionStore = {
       instance
         .get(`/articles/${id}`)
         .then((res) => {
-          commit('SET_OPINIONDETAIL', res.data);
+          commit('SET_OPINION_DETAIL', res.data);
+          commit('SET_OPINION_COMMENT', res.data.comment_set);
+          commit('SET_OPINION_COMMENT_PAGING', 0);
+        })
+        .catch((err) => console.log(err.response));
+    },
+
+    // 댓글 등록
+    opinionCommentCreate({ dispatch, state }, data) {
+      instance
+        .post(`/articles/${state.opinionData.id}/comments/`, data)
+        .then(() => {
+          dispatch('opinionDetail', state.opinionData.id);
         })
         .catch((err) => console.log(err.response));
     },
