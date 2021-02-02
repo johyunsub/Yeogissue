2fb69d727dc7133cc5940e8f49b94e33f47ba42a
GooWeonKang <fkdldjs487@gmail.com>
2021-02-03 02:16:43 +0900

    Update : 로그인 하실 vuex에 Token과 user 정보 저장, 새로고침 해도 localStorage 확인해서 vuex 정보 유지

--- front/hell/src/store/index.js
+++ front/hell/src/store/index.js
@@ -3,18 +3,27 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+import { createInstance } from '../api/index.js';
+const instance = createInstance();
+
 import opinionStore from './modules/opinionStore';
 import clubStore from './modules/clubStore';
 
 export default new Vuex.Store({
   state: {
+    //로그인 정보
+    isLoginToken: '',
+
+    // 유저 정보
+    userInfo: {},
+
     //Meun 상태
     drawer: false,
     dialog: false,
+
+    //클럽 모달 변수
     clubDialog: false,
     clubDetailUrlDialog: false,
-
-    //Club 리스트 페이지
   },
   getters: {
     getClubs(state) {
@@ -22,6 +31,18 @@ export default new Vuex.Store({
     },
   },
   mutations: {
+    //Login
+    SET_LOGIN_TOKEN(state, token) {
+      state.isLoginToken = token;
+      // localStorage.setItem('token', token);
+    },
+
+    //userInfo
+    SET_USER_INFO(state, data) {
+      state.userInfo = data;
+    },
+
+    //NavBar Modal
     CHANGE_DRAWER(state, drawer) {
       state.drawer = drawer;
     },
@@ -29,15 +50,24 @@ export default new Vuex.Store({
       state.dialog = dialog;
     },
 
+    //Club Modal
     CLUB_CREATE_DIALOG(state, dialog) {
       state.clubDialog = dialog;
     },
-
     CLUB_DETAIL_URL_DIALOG(state, dialog) {
       state.clubDetailUrlDialog = dialog;
     },
   },
-  actions: {},
+  actions: {
+    userData({ commit }, data) {
+      instance
+        .post(`/accounts/get_user/`, data)
+        .then((res) => {
+          commit('SET_USER_INFO', res.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+  },
   modules: {
     opinionStore: opinionStore,
     clubStore: clubStore,
