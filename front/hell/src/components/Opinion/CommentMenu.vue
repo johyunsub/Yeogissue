35f5f41661b79bc4f56c8308417ecaa9bdccb1e7
GooWeonKang <fkdldjs487@gmail.com>
2021-02-02 18:06:13 +0900

    Update : CommentMenu

--- front/hell/src/components/Opinion/CommentMenu.vue
+++ front/hell/src/components/Opinion/CommentMenu.vue
@@ -7,6 +7,7 @@
     </template>
 
     <v-list>
+      <!-- user 정보 알아서 items 조절 -->
       <v-list-item v-for="(item, i) in items" :key="i">
         <v-list-item-title class="choice_cursor back-gray">{{ item.title }}</v-list-item-title>
       </v-list-item>
@@ -18,6 +19,7 @@
 export default {
   data: () => ({
     items: [{ title: '수정' }, { title: '삭제' }, { title: '신고' }],
+    items2: [{ title: '신고' }],
   }),
 };
 </script>
