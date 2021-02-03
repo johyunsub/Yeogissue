338f3a42e50d6b01e763e7f215f7a539ea4851d1
GooWeonKang <fkdldjs487@gmail.com>
2021-02-04 00:06:07 +0900

    Update : ClubCreate

--- front/hell/src/main.js
+++ front/hell/src/main.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import './plugins/axios'
-import App from './App.vue'
+import Vue from 'vue';
+import './plugins/axios';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import store from './store'
-import router from './router'
+import store from './store';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 window.Kakao.init('ebbcba463d0c5113870c2df8674ac8c7');
 
 new Vue({
   vuetify,
   store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
--- front/hell/src/store/modules/clubStore.js
+++ front/hell/src/store/modules/clubStore.js
@@ -9,16 +9,27 @@ const clubStore = {
   },
   getters: {},
   mutations: {
-    SET_CLUBS(state, payload) {
-      state.clubs = payload;
+    SET_CLUBS(state, club) {
+      state.clubs = club;
     },
   },
   actions: {
+    // 조회
     clubList({ commit }) {
       instance
         .get('/club/')
         .then((res) => {
           commit('SET_CLUBS', res.data);
+        })
+        .catch((err) => console.log(err.response));
+    },
+
+    // 생성
+    clubCreate({ dispatch }, data) {
+      instance
+        .post('/club/create/', data)
+        .then(() => {
+          dispatch('clubList');
         })
         .catch((err) => console.log(err.response));
     },
