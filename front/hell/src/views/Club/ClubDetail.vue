9a0ab2721750415bb6857d992337063cd4bd8f86
GooWeonKang <fkdldjs487@gmail.com>
2021-02-04 18:31:26 +0900

    Update : ClubDetail Card

--- front/hell/src/views/Club/ClubDetail.vue
+++ front/hell/src/views/Club/ClubDetail.vue
@@ -1,26 +1,11 @@
 <template>
   <v-container>
-    <h2 class="text-center mr-tp mr-bt">클럽 이름??</h2>
+    <h2 class="text-center mr-tp mr-bt display-1">Club</h2>
     <v-row>
       <v-col cols="2"></v-col>
       <v-col id="opinion_main">
         <!-- 클럽 정보 -->
-        <v-row class="mr-tp">
-          <v-sheet height="30vh" width="100%" elevation="2">
-            <v-row class="mt-3">
-              <v-col><img src="dddd" alt="이미지 넣어줘"/></v-col>
-              <v-col>
-                <v-row>
-                  클럽정보 (맴버수, 카테고리, 개설일, 마스터)
-                </v-row>
-                <v-row class="mt-3">
-                  <v-btn color="blue darken-1" text @click="OnOff()">관리</v-btn>
-                  <v-btn color="blue darken-1" text @click="OnOff()">가입하기</v-btn>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-sheet>
-        </v-row>
+        <club-detail-card />
 
         <v-divider class="my-10"></v-divider>
 
@@ -34,13 +19,21 @@
 
 <script>
 import ClubDetailContent from '../../components/Club/Detail/ClubDetailContent.vue';
+import ClubDetailCard from '../../components/Club/Detail/ClubDetailCard.vue';
+import { mapActions } from 'vuex';
 
 export default {
-  components: { ClubDetailContent },
+  components: { ClubDetailContent, ClubDetailCard },
+  computed: {},
   data: function() {
     return {};
   },
-  methods: {},
+  methods: {
+    ...mapActions('clubStore', ['clubDetail']),
+  },
+  created() {
+    this.clubDetail(this.$route.query.id);
+  },
 };
 </script>
 
