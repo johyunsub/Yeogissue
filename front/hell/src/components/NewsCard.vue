ca806c1f10bd996c8ff7cdc888cc5055afc942aa
GooWeonKang <fkdldjs487@gmail.com>
2021-02-05 03:50:25 +0900

    Update : ClubCreate

--- front/hell/src/components/NewsCard.vue
+++ front/hell/src/components/NewsCard.vue
@@ -1,8 +1,8 @@
 <template>
   <v-sheet height="12vh">
-    <div class="title mt-5 ml-2">기사제목ㅇㅇ</div>
-    <div class="body-2 mt-3 ml-2">기사내용</div>
-    <div class="body-2 mt-8 ml-2" v-if="type === 'club'">코멘트</div>
+    <div class="title mt-10 ml-2">기사제목ㅇㅇ</div>
+    <div class="body-2 mt-3 ml-2">Url : {{ urlData.url }}</div>
+    <div class="body-2 mt-8 ml-2" v-if="type === 'club'">코멘트 : {{ urlData.comment }}</div>
     <v-divider class="my-3"></v-divider>
   </v-sheet>
 </template>
@@ -10,9 +10,16 @@
 <script>
 export default {
   props: {
-    type: { Type: String },
+    type: String,
+    urlData: Object,
   },
-  methods: {},
-  created() {},
+  methods: {
+    data() {
+      return { qqqq: this.urlData };
+    },
+  },
+  created() {
+    console.log(this.data());
+  },
 };
 </script>
