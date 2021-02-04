5b5dff7effb021aa67219326544ba7f32677ea4f
GooWeonKang <fkdldjs487@gmail.com>
2021-02-05 03:51:44 +0900

    Update : ClubDetailNews 각 Url 객체

--- front/hell/src/components/Club/Detail/ClubDetailNews.vue
+++ front/hell/src/components/Club/Detail/ClubDetailNews.vue
@@ -1,12 +1,27 @@
 <template>
   <v-container>
-    <news-card :type="'club'" />
+    <news-card v-for="(urlData, n) in clubDetailUrl" :key="n" :urlData="urlData" :type="'club'" />
   </v-container>
 </template>
 
 <script>
 import NewsCard from '../../NewsCard';
+import { mapState, mapActions } from 'vuex';
+
 export default {
   components: { NewsCard },
+  computed: {
+    ...mapState('clubStore', ['clubDetailUrl']),
+  },
+  props: {
+    categoryType: { Type: String },
+  },
+  methods: {
+    ...mapActions('clubStore', ['clubDetailUrlList']),
+  },
+  created() {
+    console.log('되나?');
+    this.clubDetailUrlList();
+  },
 };
 </script>
