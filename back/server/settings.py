59fa8dd1ec80ffaef09b8df68c0f90ad43b3bb47
이대헌 <eogjs4818@naver.com>
2021-02-04 16:33:15 +0900

    update: 다시 배포 설정

--- back/server/settings.py
+++ back/server/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
+    'http://i4d108.p.ssafy.io:8080',
 ]
 
 
--- front/hell/src/config/index.js
+++ front/hell/src/config/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000/";
-// const API_BASE_URL = "http://i4d108.p.ssafy.io:8000/";
+// const API_BASE_URL = "http://127.0.0.1:8000/";
+const API_BASE_URL = "http://i4d108.p.ssafy.io:8000/";
 
 export { API_BASE_URL };
