872500521b3e42f09b3622283b4a9f564f527f3f
조현섭 <gustjq2167@naver.com>
2021-02-04 15:00:04 +0000

    Revert "Merge branch 'hyeonseop' into 'develop'"
    This reverts merge request !51

--- front/hell/src/components/bar/MenuBar.vue
+++ front/hell/src/components/bar/MenuBar.vue
@@ -5,10 +5,10 @@
         <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
       </v-list-item-avatar>
 
-        <v-list-item-content>
-          <v-list-item-title>{{ nickname }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>이름 넣어주세요</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
 
     <v-divider></v-divider>
 
@@ -31,13 +31,8 @@
 
 <script>
 import $ from 'jquery';
-import { mapState} from 'vuex';
-import axios from 'axios';
-import { API_BASE_URL } from "../../config";
-
 export default {
-computed: {
-  ...mapState('index', ['userData']),
+  computed: {
     getDrawer: {
       get: function() {
         return this.$store.state.drawer;
@@ -47,31 +42,24 @@ computed: {
   },
   data() {
     return {
-        items:[
-          { title: 'Home', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-forum' },
-        ],
-        nickname: '',
-
-      }
-    },
-    
-    created(){
-        axios.post(`${API_BASE_URL}accounts/get_user/`, { email: localStorage.getItem('email')})
-        .then((res) => {
-          this.nickname = res.data.nickname
-        })
-        
-    },
-    updated(){
-      // 바탕화면 누르면 vuex 값을 바꿔주기 위해
-      $(document).ready(function() {
-        $('.v-overlay').on('click', function() {
-          $("#chagemenu").trigger("click");
-        });
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'About', icon: 'mdi-forum' },
+      ],
+    };
+  },
+  updated() {
+    // 바탕화면 누르면 vuex 값을 바꿔주기 위해
+    $(document).ready(function() {
+      $('.v-overlay').on('click', function() {
+        $('#chagemenu').trigger('click');
       });
-
-      
+    });
+  },
+  methods: {
+    Out: function() {
+      this.$store.dispatch('userLogout');
+      this.$store.commit('CHANGE_DRAWER', false);
     },
     OnOffMenu: function() {
       this.$store.commit('CHANGE_DRAWER', false);
