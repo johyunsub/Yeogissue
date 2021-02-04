f1ef3d2e399ff9718afd6fbc203f75692c54f3b5
GooWeonKang <fkdldjs487@gmail.com>
2021-02-04 10:55:33 +0900

    Update : Logout

--- front/hell/src/components/bar/MenuBar.vue
+++ front/hell/src/components/bar/MenuBar.vue
@@ -1,76 +1,69 @@
 <template>
-    <v-navigation-drawer
-      v-model="getDrawer"
-      absolute
-      temporary
-      right
-      width="50vh"
-    >
-      <v-list-item>
-        <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
-        </v-list-item-avatar>
+  <v-navigation-drawer v-model="getDrawer" absolute temporary right width="50vh">
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title>이름 넣어주세요</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-btn text color="red" @click="Out">로그아웃</v-btn>
+
+    <v-list dense>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
 
         <v-list-item-content>
-          <v-list-item-title>이름 넣어주세요</v-list-item-title>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <button style="display:none" id="chagemenu" @click="OnOffMenu">ddd</button>
-    </v-navigation-drawer>
+    </v-list>
+    <button style="display:none" id="chagemenu" @click="OnOffMenu">ddd</button>
+  </v-navigation-drawer>
 </template>
 
 <script>
 import $ from 'jquery';
 export default {
-computed: {
+  computed: {
     getDrawer: {
-      get: function () {
-        return this.$store.state.drawer
+      get: function() {
+        return this.$store.state.drawer;
       },
-      set: function () {
-        }
-      }
+      set: function() {},
     },
-data () {
+  },
+  data() {
     return {
-        items:[
-          { title: 'Home', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-forum' },
-        ],
-      }
-    },
-    updated(){
-      // 바탕화면 누르면 vuex 값을 바꿔주기 위해
-      $(document).ready(function() {
-        $('.v-overlay').on('click', function() {
-          $("#chagemenu").trigger("click");
-        });
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'About', icon: 'mdi-forum' },
+      ],
+    };
+  },
+  updated() {
+    // 바탕화면 누르면 vuex 값을 바꿔주기 위해
+    $(document).ready(function() {
+      $('.v-overlay').on('click', function() {
+        $('#chagemenu').trigger('click');
       });
-    },
-    methods:{
-      OnOffMenu: function() {
+    });
+  },
+  methods: {
+    Out: function() {
+      this.$store.dispatch('userLogout');
       this.$store.commit('CHANGE_DRAWER', false);
-      console.log(this.$store.state.drawer);
     },
-    }
-  }
-
+    OnOffMenu: function() {
+      this.$store.commit('CHANGE_DRAWER', false);
+    },
+  },
+};
 </script>
