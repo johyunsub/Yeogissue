1cba3df1e00a1ce7fc4ac5b3e5c838ffbe967933
GooWeonKang <fkdldjs487@gmail.com>
2021-02-05 19:37:24 +0900

    Update : mr 조정

--- front/hell/src/components/Club/Detail/ClubDetailCard.vue
+++ front/hell/src/components/Club/Detail/ClubDetailCard.vue
@@ -15,7 +15,7 @@
           <div>
             <v-row class="mt-1 mr-auto mb-2">
               <v-col cols="auto" class="mr-auto" md="10">
-                <v-sheet height="20vh" width="90%">
+                <v-sheet width="90%">
                   <!-- 클럽정보 (맴버수, 카테고리, 개설일, 마스터) -->
                   <div class="headline">{{ clubData.title }}</div>
                   <div class="mt-3">카테고리 - {{ clubData.category }}</div>
@@ -31,11 +31,11 @@
             <v-col cols="auto" class="mr-auto"></v-col>
             <div>
               <v-col cols="auto">
-                <div v-if="clubDetailManegerBtn === false">
+                <div class="mr-3" v-if="clubDetailManegerBtn === false">
                   <v-btn color="blue darken-1" text @click="managerOnOff(true)">관리</v-btn>
                   <v-btn color="blue darken-1" text @click="OnOff()">가입하기</v-btn>
                 </div>
-                <div v-if="clubDetailManegerBtn === true">
+                <div class="mr-3" v-if="clubDetailManegerBtn === true">
                   <v-btn color="blue darken-1" text @click="managerOnOff(false)">홈으로</v-btn>
                 </div>
               </v-col>
