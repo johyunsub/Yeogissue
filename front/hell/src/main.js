fc3cdf8a2abafecfd2f4a5775d10bbe2c20400ab
DESKTOP-KVCQHCD\multicampus <adkgold77@naver.com>
2021-02-06 17:20:58 +0900

    Update : 로그인 회원가입 관련 끝

--- front/hell/src/main.js
+++ front/hell/src/main.js
@@ -7,10 +7,15 @@ import router from './router';
 
 Vue.config.productionTip = false;
 window.Kakao.init('ebbcba463d0c5113870c2df8674ac8c7');
+// Vue.$cookies.config("1d");
 
 new Vue({
   vuetify,
   store,
-  router,
+  router, 
+  // beforeCreate() {
+  //   console.log('gdgd')
+  //   this.$store.dispatch("userData")
+  // },
   render: (h) => h(App),
 }).$mount('#app');
