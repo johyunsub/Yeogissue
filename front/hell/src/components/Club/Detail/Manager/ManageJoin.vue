eb67e774a9ed95518c30885b5ccd6d1878859b14
DESKTOP-KVCQHCD\multicampus <hyunsub2167@gmail.com>
2021-02-07 21:53:58 +0900

    Update : 클럽멤버 관리

--- front/hell/src/components/Club/Detail/Manager/ManageJoin.vue
+++ front/hell/src/components/Club/Detail/Manager/ManageJoin.vue
@@ -1,5 +1,25 @@
 <template>
-    <h1>가입요청</h1>
+    <v-container>
+    <v-card>
+      <v-card-title>
+        가입 요청
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="member"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+  </v-container>
+    
 </template>
 <script>
 export default {
