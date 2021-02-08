8a1ebb5f2d5d2edced6c835bc039ee4b6439988a
DESKTOP-KVCQHCD\multicampus <fkdldjs487@gmail.com>
2021-02-08 20:40:19 +0900

    Update : IssueDeatil ( 뉴스 유튜브 보여주기 전까지 )

---
+++ front/hell/src/components/Issue/IssueNewsList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row>
+      <v-col md="4"></v-col>
+      <v-col md="4" offset-md="4"></v-col>
+    </v-row>
+    <v-row> </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: ["Foo", "Bar", "Fizz", "Buzz"],
+  }),
+  methods: {
+    select() {
+      console.log("dd");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.newsSelect {
+  width: 100px;
+  height: 30px;
+  border: 1px solid #999;
+  border-radius: 3px;
+  text-align: center;
+}
+</style>
