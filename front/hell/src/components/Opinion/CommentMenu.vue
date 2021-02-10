3cf551aba6a0d02d97fc3088048fb290164b0094
GooWeonKang <fkdldjs487@gmail.com>
2021-02-10 19:45:05 +0900

    Update : CommentDelete

--- front/hell/src/components/Opinion/CommentMenu.vue
+++ front/hell/src/components/Opinion/CommentMenu.vue
@@ -6,20 +6,62 @@
       </v-btn>
     </template>
 
-    <v-list>
-      <!-- user 정보 알아서 items 조절 -->
-      <v-list-item v-for="(item, i) in items" :key="i">
-        <v-list-item-title class="choice_cursor back-gray">{{ item.title }}</v-list-item-title>
-      </v-list-item>
-    </v-list>
+    <div v-if="!isLoginToken == ''">
+      <v-list>
+        <v-list-item
+          class="choice_cursor back-gray"
+          v-for="(item, i) in items"
+          :key="i"
+          @click="updateComment(item.title, id)"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </div>
+
+    <div v-if="isLoginToken == ''">
+      <v-list>
+        <v-list-item
+          class="choice_cursor back-gray"
+          v-for="(item, i) in items2"
+          :key="i"
+          @click="updateComment(item.title, id)"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </div>
   </v-menu>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
 export default {
+  props: {
+    id: { Type: String },
+  },
+  computed: {
+    ...mapState(['isLoginToken', 'userInfo']),
+  },
   data: () => ({
     items: [{ title: '수정' }, { title: '삭제' }, { title: '신고' }],
     items2: [{ title: '신고' }],
   }),
+  methods: {
+    ...mapActions('opinionStore', ['opinionCommentDelete']),
+    updateComment(content, id) {
+      if (this.isLoginToken == '') {
+        this.$store.commit('CHANGE_DIALOG', true);
+        return;
+      }
+
+      if (content == '수정') {
+        this.$emit('CommentUp');
+      } else if (content == '삭제') {
+        this.opinionCommentDelete(id);
+      }
+      // else if (content == '신고')
+    },
+  },
 };
 </script>
