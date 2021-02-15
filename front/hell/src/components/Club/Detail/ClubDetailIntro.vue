671a54f05654cd308db3ab8a4496cbfdb767731e
DESKTOP-KVCQHCD\multicampus <hyunsub2167@gmail.com>
2021-02-15 23:59:44 +0900

    Update : 클럽디테일 css

--- front/hell/src/components/Club/Detail/ClubDetailIntro.vue
+++ front/hell/src/components/Club/Detail/ClubDetailIntro.vue
@@ -1,20 +1,20 @@
 <template>
   <v-container>  <div>
-            <v-row class="mt-1 mr-auto mb-2">
-              <v-col cols="2" class="mr-auto" md="10">
-                <!-- <v-sheet width="90%"> -->
-                  <!-- 클럽정보 (맴버수, 카테고리, 개설일, 마스터) -->
+      <v-row class="mt-1 mr-auto mb-2">
+        <v-col cols="2" class="mr-auto" md="10">
+          <!-- <v-sheet width="90%"> -->
+            <!-- 클럽정보 (맴버수, 카테고리, 개설일, 마스터) -->
 
-                  <div class="mt-3">카테고리 - {{ clubData.category }}</div>
-                  <div class="mt-3">멤버수 - {{ clubData.member_cnt }}</div>
-                  <div class="mt-3">클럽장 - {{ clubData.mastername }}</div>
-                  <div class="mt-3">개설일 - {{ clubData.created_at }}</div>
-                <!-- </v-sheet> -->
-              </v-col>
-             
-              <v-col cols="auto"></v-col>
-            </v-row>
-          </div></v-container>
+            <div class="mt-3">카테고리 - {{ clubData.category }}</div>
+            <div class="mt-3">멤버수 - {{ clubData.member_cnt }}</div>
+            <div class="mt-3">클럽장 - {{ clubData.mastername }}</div>
+            <div class="mt-3">개설일 - {{ clubData.created_at }}</div>
+          <!-- </v-sheet> -->
+        </v-col>
+        
+        <v-col cols="auto"></v-col>
+      </v-row>
+    </div></v-container>
 </template>
 
 <script>
