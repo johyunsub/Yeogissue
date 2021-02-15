32dfdd1c0acf3fe1a12913c1ea3986242ac45433
DESKTOP-KVCQHCD\multicampus <adkgold77@naver.com>
2021-02-16 00:14:13 +0900

    Update : 마이페이지 - 내가스크랩 한글 / 내가 쓴글 조회 추가

--- front/hell/src/components/Mypage/MyWrite.vue
+++ front/hell/src/components/Mypage/MyWrite.vue
@@ -1,8 +1,153 @@
 <template>
-    <h1>내가 작성한 의견</h1>
+
+  <v-container>
+    <v-row>
+      <v-col cols="1"></v-col>
+      <v-col id="opinion_main">
+        <v-row>
+          <v-col cols="7">
+            <p class="text-h4 font-weight-black mt-2">내가 작성한 글 목록
+              <i class="fas fa-edit mx-3"></i>
+            </p>
+            
+          </v-col>
+          </v-row>
+          <hr width = "100%" color = "purple">
+        <div class="mt-10"></div>
+        <!-- 카테고리 -->
+        <!-- <opinion-category /> -->
+
+        <!-- 내용 -->
+        <v-row class="mr-tp">
+          <v-col v-if="viewType == 'list'"><opinion-table /></v-col>
+          <v-col v-if="viewType == 'card'"
+            >
+            <v-list two-line>
+            <card-list
+              v-for="(item, index) in writeData"
+              :key="`${index}_items`"
+              :data="item"
+            />
+            </v-list>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col cols="auto" class="mr-auto"></v-col>
+          <v-col cols="auto">
+                <!-- <v-btn class="ma-2 btnLC" outlined large fab color="indigo"  @click="MovePage('write')"> <v-icon>mdi-pencil</v-icon></v-btn> -->
+           <!-- <v-btn class="btnLC" color="blue" rounded @click="MovePage('write')"> <v-icon color="white">mdi-pencil</v-icon> -->
+           <!-- <span style="color: white;"> 글쓰기 </span></v-btn> -->
+          </v-col>
+        </v-row>
+
+        <!-- paging -->
+        <div class="text-center">
+          <v-pagination v-model="page" :length="pagingCnt" circle></v-pagination>
+        </div>
+      </v-col>
+
+      <div style="float: right;">
+      <v-col cols="1"></v-col>
+      </div>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
+import CardList from '../../components/Opinion/CardList.vue';
+import OpinionTable from '../../components/Opinion/OpinionTable.vue';
+// import SideList from '../../components/Opinion/SideList.vue';
+import { mapState, mapActions } from 'vuex';
+import axios from "axios";
+import { API_BASE_URL } from "../../config";
+// import OpinionCategory from '../../components/Opinion/OpinionCategory.vue';
+
 export default {
+  components: { OpinionTable, CardList, },
+  computed: {
+    ...mapState('opinionStore', ['opinionPaging', 'pagingCnt','top_hashtags']),
+    ...mapState(['userInfo']),
+  },
+  data: function() {
+    return {
+      checkbox: true,
+      cateBtn: 'center',
+      search: '',
+      viewType: 'card',
+      page: 1,
+      id: '',
+      writeData:[],
+    };
+  },
+  watch: {
+    page: function(newVal) {
+      this.$store.commit('opinionStore/SET_OPINION_PAGING', (newVal - 1) * 10);
+    },
+  },
+  methods: {
+    ...mapActions('opinionStore', ['opinionList', 'opinionDetail','hashOpinionList','hash_top10']),
     
-}
+
+    ChageType(type) {
+      this.viewType = type;
+    },
+  
+    gotoList() {
+      this.opinionList();
+    },
+
+    mywriteList() {
+      console.log("userid"+this.userInfo.id);
+      
+      axios
+        .post(`${API_BASE_URL}articles/my_articles/`, { user: this.userInfo.id})
+        .then((res) => {
+          console.log(res.data);
+          this.writeData = res.data;
+        })
+        .catch((err) => console.log(err.response));
+    },
+  },
+  created() {
+    this.mywriteList();
+    this.hash_top10();
+  },
+};
 </script>
+
+<style lang="scss">
+#opinion_main {
+  margin-top: 20px;
+  margin-right: 10px;
+  // border-right: 1px solid rgb(238, 238, 238);
+  padding-right: 20px;
+  padding-top: 5px;
+}
+
+#opinion_main_content {
+  margin-top: 100px;
+}
+
+.btnLC {
+  margin-top: 20px;
+  border: 1px solid rgb(238, 238, 238);
+}
+
+// .sticky {
+//   display: inline-block;
+//   position: sticky;
+//   background: blue;
+// }
+
+// .scroll-downs {
+//   position: absolute;
+// }
+
+.v-btn--example {
+    bottom: 0;
+    position:fixed;
+    margin: 0 0 16px 16px;
+    right: 100;
+  }
+</style>
