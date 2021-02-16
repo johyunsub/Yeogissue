8698eb24cba80c47b37ddd57f27bf21ed12cc500
GooWeonKang <fkdldjs487@gmail.com>
2021-02-16 14:35:17 +0900

    ㅇ

--- back/club/urls.py
+++ back/club/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('myclub/', views.myclub),
     path('club_image/<str:club_pk>/', views.club_image),
     path('', views.club_list),
@@ -19,21 +18,4 @@ urlpatterns = [
     path('member_delete/<str:club_pk>/<str:member_id>/', views.member_delete),
     path('member_check/<str:club_pk>/', views.member_check),
     path('club_member_delete/<str:club_pk>/', views.club_member_delete),
-=======
-    # path('myclub/',views.myclub),
-    # path('club_image/<str:club_pk>/',views.club_image),
-    path('',views.club_list),
-    path('create/',views.club_create),
-    path('club_detail/<str:club_pk>/',views.club_detail),
-    path('club_article/',views.club_article),
-    path('club_article_list/<str:club_pk>/news/',views.club_article_list_news),
-    path('club_article_list/<str:club_pk>/youtube/',views.club_article_list_youtube),
-    path('club_article_list/<str:club_pk>/etc/',views.club_article_list_etc),
-    path('club_article_detail/<str:club_article_pk>/',views.club_article_detail),
-    path('club_signup/<str:club_pk>/',views.club_signup),
-    path('member_approve/<str:club_pk>/',views.member_approve),
-    path('member_delete/<str:club_pk>/<str:member_id>/',views.member_delete),
-    path('member_check/<str:club_pk>/',views.member_check),
-    path('club_member_delete/<str:club_pk>/',views.club_member_delete),
->>>>>>> d28083bdec110e7d5b6eac0b99128ec10d750014
 ]
--- front/hell/src/components/Club/Detail/ClubDetailCard.vue
+++ front/hell/src/components/Club/Detail/ClubDetailCard.vue
@@ -21,96 +21,15 @@
             />
           </div>
         </v-col>
-<<<<<<< HEAD
         <v-col cols="auto">
           <v-btn class="btnLC" text color="blue darken-1" @click="isClubJoin">
             <i class="fas fa-chevron-circle-up"> </i>URL 등록</v-btn
           >
         </v-col>
         <v-col cols="auto">
-          <div class="mt-5 ml-1">
-            <div v-if="clubDetailManegerBtn === false">
-              <v-btn
-                v-if="clubData.master == userInfo.id"
-                color="blue darken-1"
-                text
-                @click="managerOnOff(true)"
-              >
-                <v-icon color="blue">fas fa-user-cog</v-icon>
-                <span style="color: blue;">관리 </span>
-              </v-btn>
-            </div>
-            <v-btn
-              v-if="clubData.master != userInfo.id && clubDetailIsMember"
-              color="blue darken-1"
-              text
-              @click="doLeave()"
-              >클럽탈퇴</v-btn
-            >
-            <v-btn
-              v-if="clubData.master != userInfo.id && !clubDetailIsMember && clubDetailIsWaiting"
-              color="blue darken-1"
-              text
-              >가입대기중</v-btn
-            >
-            <div @click="loginCheck">
-              <v-dialog
-                v-if="clubData.master != userInfo.id && !clubDetailIsMember && !clubDetailIsWaiting"
-                transition="dialog-bottom-transition"
-                max-width="600"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn color="blue darken-1" text v-bind="attrs" v-on="on">
-                    <v-icon color="blue">fas fa-user-friends</v-icon>
-                    <span style="color: blue;">가입하기</span>
-                  </v-btn>
-                </template>
-                <template v-slot:default="dialog">
-                  <v-card>
-                    <v-toolbar color="primary" dark> {{ clubData.title }}클럽 가입신청서</v-toolbar>
-                    <v-card-text>
-                      <div class="text-h6 font-weight-black pa-12">
-                        반가워요 {{ userInfo.nickname }}님.
-                      </div>
-                    </v-card-text>
-                    <v-sheet class="mx-8">
-                      <v-textarea
-                        class="px-3"
-                        label="가입동기를 적어주세요"
-                        auto-grow
-                        outlined
-                        rows="4"
-                        row-height="30"
-                        shaped
-                        v-model="content"
-                      ></v-textarea>
-                    </v-sheet>
-                    <v-card-actions class="justify-end">
-                      <v-div @click="dialog.value = false">
-                        <v-btn text @click="goJoin">제출</v-btn>
-                      </v-div>
-                      <v-btn text @click="dialog.value = false">닫기</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </template>
-              </v-dialog>
-            </div>
-            <div v-if="clubDetailManegerBtn === true && clubData.master == userInfo.id">
-              <v-btn color="blue darken-1" text @click="managerOnOff(false)">홈으로</v-btn>
-            </div>
-          </div>
-=======
-
-        <v-col cols="auto">
-          <span
-            ><v-btn class="btnLC" text color="blue darken-1" @click="isClubJoin">
-              <i class="fas fa-chevron-circle-up"> </i>URL 등록</v-btn
-            ></span
-          >
-          <span v-if="clubDetailManegerBtn === false">
+          <div v-if="clubDetailManegerBtn === false">
             <v-btn
               v-if="clubData.master == userInfo.id"
-              class="mt-4 ml-2"
               color="blue darken-1"
               text
               @click="managerOnOff(true)"
@@ -118,7 +37,7 @@
               <v-icon color="blue">fas fa-user-cog</v-icon>
               <span style="color: blue;">관리 </span>
             </v-btn>
-          </span>
+          </div>
           <v-btn
             v-if="clubData.master != userInfo.id && clubDetailIsMember"
             color="blue darken-1"
@@ -172,12 +91,9 @@
               </v-card>
             </template>
           </v-dialog>
-          <span v-if="clubDetailManegerBtn === true && clubData.master == userInfo.id">
-            <v-btn class="mt-4 ml-2" color="blue darken-1" text @click="managerOnOff(false)"
-              >홈으로</v-btn
-            >
-          </span>
->>>>>>> d28083bdec110e7d5b6eac0b99128ec10d750014
+          <div v-if="clubDetailManegerBtn === true && clubData.master == userInfo.id">
+            <v-btn color="blue darken-1" text @click="managerOnOff(false)">홈으로</v-btn>
+          </div>
         </v-col>
       </v-row>
     </v-sheet>
@@ -227,10 +143,6 @@ export default {
         type: 'success',
       });
     },
-<<<<<<< HEAD
-    loginCheck() {},
-=======
->>>>>>> d28083bdec110e7d5b6eac0b99128ec10d750014
     isClubJoin() {
       if (this.clubDetailIsMember || this.userInfo.id == this.clubData.master) {
         this.OnOff();
